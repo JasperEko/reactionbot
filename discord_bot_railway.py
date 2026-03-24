86c247ea964266a9ff4c39b5677b8c6d314a273b
JasperEko <66704379+JasperEko@users.noreply.github.com>
2026-03-24 16:51:10 -0700

    Update discord_bot_railway.py

--- discord_bot_railway.py
+++ discord_bot_railway.py
@@ -1,18 +1,45 @@
-import asyncio
 import os
 import discord
-import requests
-WEBHOOK = os.environ["WEBHOOK"]
-TOKEN = os.environ["TOKEN"]
+from discord.ext import commands
+import random
+
+# Create bot
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
-@client.event
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Your phrases and images
+items = [
+  "Hey emo boy. Hey hey hey emo boy",
+  "DAVID BASZUCKI...",
+  "yall think disneys chicken little is a 10/10 game",
+  "i wish i was high on potenuse",
+  "bro lookin like an ipad kid 💀💀💀💀💀☠️☠️☠️",
+  "im spongebob im spongebob",
+  "i do that too",
+  "a wacky spin on a popular classic!",
+  "charlie kirk in mha: here's my charlie quirk",
+  "DELTARUNE BACKWARDS IS UNDERTALE",
+  "mmm yummy slop",
+  "https://image2url.com/r2/default/images/1773561788341-c382783b-170f-4e15-b3dd-496b010aaf26.png",
+  "https://image2url.com/r2/default/images/1773894962347-e2b7a674-9471-4d37-bffb-2b941f290815.jpg",
+]
+
+@bot.event
 async def on_ready():
-    print(f"Logged in as {client.user}")
-@client.event
-async def on_message(msg):
-    if msg.author == client.user or "react" not in msg.content.lower():
+    print(f'{bot.user} has connected to Discord!')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
         return
-    asyncio.create_task(asyncio.to_thread(requests.post, WEBHOOK, json={"text": "react"}))
-client.run(TOKEN)
+
+    if message.content.lower() == "react":
+        random_item = random.choice(items)
+
+        if random_item.startswith("http"):
+            await message.channel.send(embed=discord.Embed().set_image(url=random_item))
+        else:
+            await message.channel.send(random_item)
+
+bot.run(os.environ["TOKEN"])
